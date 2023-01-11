8647a4154f513bf452326dcc885ea3ba10954550
moeyassein <yassein75@gmail.com>
2023-01-10 20:55:14 -0500

    add wooden spoon

--- src/day1/FirstClass.java
+++ src/day1/FirstClass.java
@@ -2,6 +2,7 @@ package day1;
 
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("Hollow world");
+        System.out.println("Hello world");
+        System.out.println("wooden spoon");
     }
 }
